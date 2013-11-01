c6e3ccfd5737099c480b47151ecb85a4e48c9ce4
Muhammad S. Maromi <msmaromi@gmail.com>
2013-11-01 23:47:21 +0700

    updateee

--- MyAd/src/myad/View.java
+++ MyAd/src/myad/View.java
@@ -32,27 +32,42 @@ public class View extends JFrame {
   }
   
   private void initComponents() throws Exception {
-    XMLReader rd = new XMLReader("ad.xml");
-    int size = rd.contentSize();
-    System.out.println(size);
-    ArrayList<JLabel> labelsKey = new ArrayList<JLabel>(size);
-    ArrayList<JLabel> labelsVal = new ArrayList<JLabel>(size);
-    for (int i = 0; i < size; i++) {
-      System.out.println(i);
-      String key = rd.getKey(i);
+    XMLReader rd = new XMLReader("my.xml");
+    System.out.println(rd.getRole());
+    System.out.println(rd.getName());
+//    for (int i = 0; i < 2; i++) {      
+//      System.out.println(rd.Phone(i+1, "Type"));
+//      System.out.println(rd.Phone(i+1, "PhoneNumber"));
+//      System.out.println(rd.Phone(i+1, "AreaCode"));
+//      System.out.println(rd.Phone(i+1, "SubscriberNo"));
+//    }
+    
+    System.out.println(rd.Address("Street"));
+    System.out.println(rd.Address("ZipPostalCode"));
+    System.out.println(rd.Address("City"));
+    System.out.println(rd.Address("StateProvince"));
+    System.out.println(rd.Address("CountryName"));
+    
+//    int size = rd.contentSize();
+//    System.out.println(size);
+//    ArrayList<JLabel> labelsKey = new ArrayList<JLabel>(size);
+//    ArrayList<JLabel> labelsVal = new ArrayList<JLabel>(size);
+//    for (int i = 0; i < size; i++) {
+////      System.out.println(i);
+//      String key = rd.getKey(i);
+//      
+//      labelsKey.add(new JLabel(key));
+//      labelsVal.add(new JLabel(rd.getValue(key)));                                        
       
-      labelsKey.add(new JLabel(key));
-      labelsVal.add(new JLabel(rd.getValue(key)));                                        
-      
-      int gap = i*1;
-      Insets insets = panel.getInsets();      
-      
-      labelsKey.get(i).setLocation(10, 10+0);
-      labelsVal.get(i).setLocation(30, 10+0);
-      
-      panel.add(labelsKey.get(i));
-      panel.add(labelsVal.get(i)); 
-    }        
+//      int gap = i*1;
+//      Insets insets = panel.getInsets();      
+//      
+//      labelsKey.get(i).setLocation(10, 10+0);
+//      labelsVal.get(i).setLocation(30, 10+0);
+//      
+//      panel.add(labelsKey.get(i));
+//      panel.add(labelsVal.get(i)); 
+//    }        
     
   }
   
